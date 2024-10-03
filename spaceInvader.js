366eb98a359065a9100512901e1f5f68e200b6a9
NoahGoossens-ehb <noah.goossens@student.ehb.be>
2024-10-03 15:13:21 +0200

    changes done

--- spaceInvader.js
+++ spaceInvader.js
@@ -16,64 +16,16 @@ function spaceInvader(){
     context.beginPath();
     context.fillStyle = "#83f52c";
     context.rect(175,75,50,50);
-    context.fill();
-    
-    context.beginPath();
-    context.fillStyle = "#83f52c";
     context.rect(125,125,50,50);
-    context.fill();
-    
-    context.beginPath();
-    context.fillStyle = "#83f52c";
     context.rect(225,125,50,50);
-    context.fill();
-
-    context.beginPath();
-    context.fillStyle = "#83f52c";
     context.rect(225,175,50,50);
-    context.fill();
-
-    context.beginPath();
-    context.fillStyle = "#83f52c";
     context.rect(125,175,50,50);
-    context.fill();
-
-    context.beginPath();
-    context.fillStyle = "#83f52c";
     context.rect(175,225,50,50);
-    context.fill();
-
-    context.beginPath();
-    context.fillStyle = "#83f52c";
     context.rect(75,175,50,50);
-    context.fill();
-
-
-    context.beginPath();
-    context.fillStyle = "#83f52c";
     context.rect(75,225,50,50);
-    context.fill();
-
-    context.beginPath();
-    context.fillStyle = "#83f52c";
     context.rect(75,275,50,50);
-    context.fill();  
-
-    
-    context.beginPath();
-    context.fillStyle = "#83f52c";
     context.rect(275,175,50,50);
-    context.fill();
-
-    context.beginPath();
-    context.fillStyle = "#83f52c";
     context.rect(275,225,50,50);
-    context.fill();
-
-    context.beginPath();
-    context.fillStyle = "#83f52c";
     context.rect(275,275,50,50);
     context.fill();
-
-
 }
