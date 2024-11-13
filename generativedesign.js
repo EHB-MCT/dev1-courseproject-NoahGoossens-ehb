1b03c773117ed4376297f045ae883415d3cebc44
NoahGoossens-ehb <noah.goossens@student.ehb.be>
2024-11-13 12:41:44 +0100

    const voor kleuren

--- generativedesign.js
+++ generativedesign.js
@@ -1,6 +1,7 @@
 "use strict";
 import context from "../../dev1-courseproject-NoahGoossens-ehb/dev1-courseproject-NoahGoossens-ehb/scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
+import { hsl, randomGaussian, randomNumber } from "./scripts/utils.js";
 
 
 let width = context.canvas.width;
@@ -10,4 +11,16 @@ let height = context.canvas.height;
 function sizeCanvas() {
     width = window.innerWidth * 0,8; //verkleine naar 80%
     height = window.innerHeight * 0,8; // verkleine naar 80%
+}
+
+function draw() {
+
+    //const voor kleuren random//
+    const colors = {
+        background : hsl(randomNumber(0,360),20,95),
+        color1: hsl(randomNumber(0,360),70,60),
+        color2:  hsl(randomNumber(0,360),70,60),
+        color3:  hsl(randomNumber(0,360),70,50),
+        color4:  hsl(randomNumber(0,360),70,40)
+    }
 }
