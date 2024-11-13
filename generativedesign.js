86de04cfbcc813bf7041df1bc91f0cdd9f17ec0a
NoahGoossens-ehb <noah.goossens@student.ehb.be>
2024-11-13 12:33:20 +0100

    aanmaken scripts file, op mac, sizing van canvs naar 80%

--- generativedesign.js
+++ generativedesign.js
@@ -1,4 +1,13 @@
 "use strict";
-import context from "../../scripts/context.js";
+import context from "../../dev1-courseproject-NoahGoossens-ehb/dev1-courseproject-NoahGoossens-ehb/scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+
+function sizeCanvas() {
+    width = window.innerWidth * 0,8; //verkleine naar 80%
+    height = window.innerHeight * 0,8; // verkleine naar 80%
+}
