77120e97453b05454afaaff80e21cf1afeb83fa3
Noah Goossens <noah.goossens@student.ehb.be>
2024-12-19 00:52:41 +0100

    grasveld in heuvels

--- interactivedesign.js
+++ interactivedesign.js
@@ -17,7 +17,26 @@ function tekenAchtergrond() {
 
    //grasveld
    
-   context.fillStyle = Utils.hsl(120,50,40);
-   context.fillRect(0,0,width,height * 0.3); //
+   const kleurGras = Utils.hsl(120, 50, 40);
+   context.fillStyle = kleurGras;
+   
+   //heuvels van grasveld voor diepte
+   context.beginPath();
+   context.moveTo(0, canvas.height * 0.7);
+
+   //eerste heuvel
+   Utils.fillEllipse(canvas.width * 0.2, canvas.height * 0.8, 800, 330);
+
+   //tweede heuvel
+   Utils.fillEllipse(canvas.width * 0.5, canvas.height * 0.75, 980, 200);
+
+   //derde heuvel
+   Utils.fillEllipse(canvas.width * 0.8, canvas.height * 0.7, 700, 400);
+
+   
+
+   
+
+
 }
 
