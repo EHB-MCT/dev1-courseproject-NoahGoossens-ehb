47606514eb7b54b1c1dd1bc3d3f3aadce1ab3dcf
Noah Goossens <noah.goossens@student.ehb.be>
2024-12-19 00:45:06 +0100

    achtergrond werkte niet, error in de import van utils en context

--- interactivedesign.js
+++ interactivedesign.js
@@ -1,26 +1,23 @@
 "use strict";
-import context from "../../scripts/context.js";
-import * as Utils from "../../scripts/utils.js";
+import context from "/scripts/context.js";
+import * as Utils from "/scripts/utils.js";
 
+const width = context.canvas.width;
+const height = context.canvas.height;
 
+tekenAchtergrond();
 
 //achtergrond functies instellen
 function tekenAchtergrond() {
    //kleur instellen voor achtergrond  
-   const kleurHemel = hsl(210 , 50 ,10);
-   context.fillStyle = kleurHemel; 
-   context.fillRect(0,0,canvas.width,canvas.height * 0.7); //70% is lucht 30 is land (gras)
+
+   context.fillStyle = Utils.hsl(210 , 50 ,10);
+   context.fillRect(0,0,width,height * 0.7); //70% is lucht 30 is land (gras)
+
+
+   //grasveld
+   
+   context.fillStyle = Utils.hsl(120,50,40);
+   context.fillRect(0,0,width,height * 0.3); //
 }
 
-//grasveld
-function tekenGrasveld() {
-
-}
-
-//sterren
-function tekenSterren() {
-
-}
-
-
-tekenAchtergrond();
