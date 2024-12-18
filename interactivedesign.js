f5c895e6f775c1f40fe870c60534613df33bcfbf
Noah Goossens <noah.goossens@student.ehb.be>
2024-12-19 00:11:40 +0100

    instellen volgorde achtergrond

--- interactivedesign.js
+++ interactivedesign.js
@@ -2,6 +2,18 @@
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 
+
+//achtergrond functies instellen
 function tekenAchtergrond() {
-    
+   
+}
+
+//grasveld
+function tekenGrasveld() {
+
+}
+
+//sterren
+function tekenSterren() {
+
 }
